042cd9c24fdb80d15b7f19015fa07931d2ecf112
Edwin John Flores <edwin.flores@klab.com>
2014-12-15 11:14:55 +0800

    Added comments feature and exclusive viewing of threads

--- app/controllers/thread_controller.php
+++ app/controllers/thread_controller.php
@@ -1,7 +1,18 @@
 <?php
-class ThreadController extends AppController {
-    public function index(){
-        //TODO: Get all threads
+class ThreadController extends AppController 
+{
+    public function index()
+    {
+        $threads = Thread::getAll();
+
+        $this->set(get_defined_vars());
+    }
+
+    public function view()
+    {
+        $thread = Thread::get(Param::get('thread_id'));
+        $comments = $thread->getComments();
+
         $this->set(get_defined_vars());
     }
 }
---
+++ app/views/thread/view.php
@@ -0,0 +1,12 @@
+<h1><?php eh($thread->title) ?></h1>
+<?php foreach ($comments as $k => $v): ?>
+	<div class="comment">
+
+		<div class="meta">
+			<?php eh($k + 1) ?>: <?php eh($v->username) ?> <?php eg($v->created) ?>
+		</div>
+
+		<div><?php eh($v->body) ?> </div>
+
+	</div>
+<?php endforeach ?>
