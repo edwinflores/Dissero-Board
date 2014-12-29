0606997a2670e88c5d4ed02fb9b38e6e22e0664a
Edwin John Flores <edwin.flores@klab.com>
2014-12-29 17:13:36 +0800

    Removed unnecessary debug echo and removed nested loop

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -1,14 +1,12 @@
 <h2><font style="font-size: 50px">Log-in Screen</font></h2><br />
 
 <?php
-    if(isset($user)){
-        if (!$user->isLoginValid()):
-            echo $user->isLoginValid();
+    if (!$user->isLoginValid()):
     ?>
     <div class="alert">
         <div>Invalid Username and/or Password</div>
     </div>
-<?php endif; }  ?>
+<?php endif; ?>
 
 <form class="well" method="POST" action="<?php encodeString(url('user/login'))?>">
     <label><font style="font-size: 20px">Username:</font></label>
